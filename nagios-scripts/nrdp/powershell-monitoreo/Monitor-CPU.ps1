71c5e7870acbbfed2e5a430d65eac47aa18517c9
Cayu <408436+cayu@users.noreply.github.com>
2026-01-21 23:07:10 -0300

    Update Monitor-CPU.ps1

--- nagios-scripts/nrdp/powershell-monitoreo/Monitor-CPU.ps1
+++ nagios-scripts/nrdp/powershell-monitoreo/Monitor-CPU.ps1
@@ -11,7 +11,8 @@ try {
     $cpu = (Get-CimInstance Win32_Processor | Measure-Object -Property LoadPercentage -Average).Average
     $cpu = [math]::Round($cpu, 1)
     if ($cpu -gt 90) { $s=2; $m="CRITICAL" } elseif ($cpu -gt 75) { $s=1; $m="WARNING" } else { $s=0; $m="OK" }
-    $out = "$m - CPU Load: $cpu%"
+#    $out = "$m - CPU Load: $cpu%"
+    $out = "$m - CPU Load: $cpu% | cpu=${cpu}%;75;90;0;100"
 } catch { $s=3; $out="UNKNOWN: $_" }
 
 Send-NRDP -NRDPUrl $Cfg.NRDPUrl -Token $Cfg.Token -User $Cfg.User -Password $Cfg.Password -Service "CPU Load" -State $s -Output $out
